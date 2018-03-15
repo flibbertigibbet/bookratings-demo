eed885ad8d9c856d70b59983ed23a3b70a6ecf1a
Kathryn Killebrew <kkillebrew@azavea.com>
2018-03-15 14:08:21 -0400

    Linting

--- ratings/forms.py
+++ ratings/forms.py
@@ -1,7 +1,5 @@
 from django import forms
-from django.contrib.auth import (
-    authenticate, get_user_model, password_validation,
-)
+from django.contrib.auth import get_user_model
 
 from django.contrib.auth.forms import UsernameField
 
