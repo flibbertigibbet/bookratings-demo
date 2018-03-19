ad749b4ea3ecf1beace7613282691495e096c23d
Kathryn Killebrew <banderkat@gmail.com>
2018-03-18 23:39:53 -0400

    Add Travis configuration

--- scripts/test.sh
+++ scripts/test.sh
@@ -1,7 +1,22 @@
 #!/bin/bash
 
-# python tests
-"${BASH_SOURCE%/*}/manage.sh" test
+set -e
+
+docker-compose up -d database django
+
+# tests
+echo $'\nRunning Django tests...'
+docker-compose \
+    run --rm --entrypoint python \
+    django manage.py test
+
+docker-compose kill django \
+    && docker-compose rm -f django
+echo $'\nDjango tests done.'
 
 # python linter
+echo $'\nRunning linter...'
 flake8 .
+echo 'Linting done.'
+
+echo $'\nTests done.'
